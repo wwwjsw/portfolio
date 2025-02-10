b167334b1103d1ecea8c823157fc174ff7626869
guilherme <progr.guilherme@gmail.com>
2025-02-10 22:51:45 +0000

    Adiciona suporte ao Tailwind CSS e configurações de pré-conexão para fontes; remove estilos CSS desnecessários e ajusta a estrutura do App

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajuste conforme sua estrutura de arquivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
